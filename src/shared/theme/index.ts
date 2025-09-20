bdc5ac21614baf0076cb1973c5ed6cbf24fbdf7c
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-20 23:49:23 +0700

    feat: add theme colors

--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -2,6 +2,10 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
   palette: {
+    error: { main: "#DC143C" },
+    info: { main: "#03a9f4" },
+    warning: { main: "#ed6c02" },
+    success: { main: "#4caf50" },
     primary: {
       main: "#6D94C5",
     },
