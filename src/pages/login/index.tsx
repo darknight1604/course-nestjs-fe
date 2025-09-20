2160f4917fba06a04b5f0125ef9ab30e7bbc81dc
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-20 22:28:18 +0700

    refactor: use style instead style inline LoginPage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,7 @@ import { Box, Container, Typography } from "@mui/material";
 import { useAtomValue } from "jotai";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
+import { styles } from "./styles";
 
 const LoginPage = () => {
   const auth = useAtomValue(authAtom);
@@ -17,14 +18,7 @@ const LoginPage = () => {
 
   return (
     <Container maxWidth="xs">
-      <Box
-        sx={{
-          mt: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+      <Box sx={styles.container}>
         <Typography component="h1" variant="h5">
           Login
         </Typography>
